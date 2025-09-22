45eddc3ad89538c56eaca96b3b6f57a30864ec6b
vaibhavninama <vaibhavninama172@gmail.com>
2025-09-22 21:40:35 +0530

    spt -22

---
+++ src/assets/pages/home/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import NavBar from '../../componet/navbar/navbar'
+
+const index = () => {
+  return (
+    <div className='bg-amber-500'>hyyy</div>
+  )
+}
+
+export default index
